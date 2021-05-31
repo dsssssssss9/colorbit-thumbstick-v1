c199bd26235219142a9382a7eb8bb33461520393
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-31 19:12:35 +0100

    sw on pin 1. x on p0 .y 0n p2. as sw not working on p0

--- main.ts
+++ main.ts
@@ -1,22 +1,28 @@
-input.onPinReleased(TouchPin.P0, function () {
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
+	
+})
+input.onPinReleased(TouchPin.P1, function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.clearScreen()
 })
 basic.forever(function () {
     led.plot(pins.map(
-    pins.analogReadPin(AnalogPin.P2),
+    pins.analogReadPin(AnalogPin.P0),
     1023,
     0,
     0,
     5
     ), pins.map(
-    pins.analogReadPin(AnalogPin.P1),
+    pins.analogReadPin(AnalogPin.P2),
     0,
     1023,
     0,
     5
     ))
-    basic.pause(50)
+    basic.pause(100)
     basic.clearScreen()
 })
